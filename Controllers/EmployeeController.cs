9eb241d07f57c6ce8d456d9041ea5c891fb32e61
Cristo <christox390@gmail.com>
2024-02-21 07:09:48 +0530

    first commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using Entity.Entity;
+using Entity.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entity.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly EntityDbContext dbContext;
+            public EmployeeController(EntityDbContext dbcontext)
+        {
+            this.dbContext = dbcontext;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Employee emp) 
+        {
+            var employee = new Employee
+            {
+                Id =emp.Id,
+                Name = emp.Name,
+                Salary = emp.Salary,
+                Email = emp.Email,
+                DateOfBirth = emp.DateOfBirth
+            };
+            dbContext.EmployeeTable.Add(employee);
+            dbContext.SaveChanges();
+            return RedirectToAction("Add");
+        }
+    }
+}
---
+++ Models/EmployeeModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Entity.Models
+{
+    public class EmployeeModel
+    {
+        public int Id { get; set; }
+        public String Name { get;set; }
+        public String Email { get; set; }
+        public long Salary { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
